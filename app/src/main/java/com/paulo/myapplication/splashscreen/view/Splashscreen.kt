97e14782007af1ecf978f462ecd87ed7fa4a5e50
paulo4fs <engpaulofrota@gmail.com>
2020-11-26 08:02:44 -0300

    criado splashscreen

---
+++ app/src/main/java/com/paulo/myapplication/splashscreen/view/Splashscreen.kt
@@ -0,0 +1,12 @@
+package com.paulo.myapplication.splashscreen.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.paulo.myapplication.R
+
+class Splashscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
