9ffa6ff0a53a8f484acfb878add31d217e15b34f
paulo4fs <engpaulofrota@gmail.com>
2020-11-26 21:31:35 -0300

    criado toolbar no signup

--- app/src/main/java/com/paulo/myapplication/login/view/LoginFragment.kt
+++ app/src/main/java/com/paulo/myapplication/login/view/LoginFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.button.MaterialButton
 import com.paulo.myapplication.R
 
 class LoginFragment : Fragment() {
@@ -18,5 +20,14 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var btnSignup = view.findViewById<MaterialButton>(R.id.btnSignupLogin)
+        btnSignup.setOnClickListener {
+            var navController = findNavController()
+            navController.navigate(R.id.action_loginFragment_to_signupFragment)
+        }
     }
 }
